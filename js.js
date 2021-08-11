7430257d8695e954e455cd7e99d20ccf66b8c154
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-08-11 13:59:58 +0100

    updated

--- js.js
+++ js.js
@@ -4,12 +4,12 @@ window.onload = () => {
     fetch("https://api.pexels.com/v1/search?query=car", {
         "method": "GET",
         "headers": {
-            "Authorization": "563492ad6f91700001000001c37b06144d9249f6907a4a1732edda7c"
+            "Authorization": "bearer 563492ad6f91700001000001a21d412f76244d6ab5f47ba8a941ecd6"
 
 
         }
     })
-    .then( respon => respon.json())
+    .then( response => response.json())
     .then(data => dataBase = data)
     .catch(err => console.error(err))
     
@@ -21,7 +21,7 @@ function check (){
     console.log(dataBase.photos[0])
 }
 
-const = getImgs = function loadImgs() {
+function loadImgs() {
     fetch("https://api.pexels.com/v1/search?query=programming") , {mode:'cors'} {
         method: "GET",
         withCredentials: true,
