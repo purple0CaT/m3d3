dba87f48189c849ef9788162a07692cdb1b30215
Istvan <steven.ford.j@gmail.com>
2021-08-11 16:07:16 +0300

    added

--- js.js
+++ js.js
@@ -22,58 +22,41 @@ function check (){
 }
 function loadPic(){
     loadingCont(dataBase.photos)
-    
+
 }
 
 function loadingCont(inf){
     for ( let card of inf){
-
     row.innerHTML += `<div class="col-md-4">
-    <div class="card mb-4 shadow-sm">
-      <svg
-        class="bd-placeholder-img card-img-top"
-        width="100%"
-        height="225"
-        xmlns="${card}"
-        preserveAspectRatio="xMidYMid slice"
-        focusable="false"
-        role="img"
-        aria-label="Placeholder: Thumbnail"
-      >
-        <title>Placeholder</title>
-        <rect width="100%" height="100%" fill="#55595c" />
-        <text x="50%" y="50%" fill="#eceeef" dy=".3em">
-          Thumbnail
-        </text>
-      </svg>
+    <div class="card mb-4 shadow-sm h-100">
+    <img class="card-img-top" src=${card.src.large} alt="Card image cap">
       <div class="card-body">
         <p class="card-text">
           This is a wider card with supporting text below as a natural
           lead-in to additional content. This content is a little bit
           longer.
         </p>
-        <div
-          class="d-flex justify-content-between align-items-center"
-        >
+        <div class="d-flex justify-content-between align-items-center">
           <div class="btn-group">
-            <button
-              type="button"
-              class="btn btn-sm btn-outline-secondary"
-            >
-              View
+            <button type="button"class="btn btn-sm btn-outline-secondary">
+                View
             </button>
-            <button
-              type="button"
-              class="btn btn-sm btn-outline-secondary"
-            >
-              Edit
+            <button onclick="hideCard(this)" type="button"  class="btn btn-sm btn-outline-secondary">
+                Hide
             </button>
-          </div>
-          <small class="text-muted">9 mins</small>
+            </div>
+            <small class="text-muted">${card.id}</small>
         </div>
       </div>
     </div>
   </div>
 `
 }
+}
+
+
+// hide card\
+function hideCard(inf){
+    let hiden = inf.parentElement.parentElement.parentElement.parentElement.parentElement
+    hiden.remove()
 }
